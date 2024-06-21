96c4d6b40d1d96452e2beb6081f1d63cb10ccd1e
NguyenVietChauNgoc <nguyenvietchaungoc@gmail.com>
2024-06-21 21:36:06 +0700

    Update Bài 4.sql

--- Bài 4.sql
+++ Bài 4.sql
@@ -1,3 +1,3 @@
 select
-ROUND(CAST(SUM(item_count*order_occurrences) / SUM(order_occurrences) DESCIMAL),1) as mean 
+ROUND(CAST(SUM(item_count*order_occurrences) / SUM(order_occurrences) DECIMAL),1) as mean 
 FROM items_per_order;
